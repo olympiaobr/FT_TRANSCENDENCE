a00f0b9af1bc8eb33d6582ae5890e7efd5185e09
Finanzcreeper <reher.nico@web.de>
2024-11-06 18:25:57 +0100

    added pols and placeholder sites for them

--- Djangotutorial/polls/views.py
+++ Djangotutorial/polls/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question nr. %s."% question_id)
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of Question nr. %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse ("you are voting on question nr. %s." %question_id)
