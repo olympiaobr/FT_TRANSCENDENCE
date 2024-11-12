33addc7a2e95a67d02a5554171ceac795b86e770
Finanzcreeper <reher.nico@web.de>
2024-11-12 18:14:37 +0100

    taken out some pygame dependancy

--- Djangotutorial/pong/models.py
+++ Djangotutorial/pong/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
-import pygame, sys
+import pygame, sys, time
 
 # Create your models here.
 def handlePaddleRMovement(event):
 
 	return 0,0
 
+#collision detection
+#def collides(a,b):
+#	if a.x =
 
 class pong(models.Model):
 	pygame.init()
 
 	#arbirary base values
 	screen_width = 1000
-	screen_height = 500
+	screen_height = screen_width / 2
 
 	#create screen (needs to be handeled by fontend eventually)
 	screen = pygame.display.set_mode((screen_width, screen_height))
@@ -30,6 +33,8 @@ class pong(models.Model):
 	ball = pygame.Rect(0,0,ball_sizex,ball_sizey)
 	paddleL = pygame.Rect(0,0,paddle_width,paddle_heigth)
 	paddleR = pygame.Rect(0,0,paddle_width,paddle_heigth)
+
+	print(ball.size)
 
 	paddleR.centery = screen_height/2
 	paddleR.centerx = screen_width - paddle_width
@@ -51,9 +56,9 @@ class pong(models.Model):
 	Lscore = 0
 	Rscore = 0
 
-	gamestate = [None] * 6
-	gamestate[4] = Lscore
-	gamestate[5] = Rscore
+	gamestate = [None] * 7
+	gamestate[5] = Lscore
+	gamestate[6] = Rscore
 	#gamestate: paddleL.y, paddleR.y, ball.x, ball.y, Lscore, Rscore
 
 	log = open("gamedata.txt", "w")
@@ -64,7 +69,19 @@ class pong(models.Model):
 	ball_speedX = ball_start_speedx
 	ball_speedY = ball_start_speedy
 
+	passes = 0
+
+	start = int(time.time() * 1000)
+
+	nonce = int(time.time() * 1000) - start
+
+	hitnbr = 0
+	speed = 120
+
 	while True:
+		if passes > 0 and passes % 2 == 0:
+			ball_speedX += 1
+			passes = 0
 		#check for events (not sure how to get that from the frontend in the end)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -79,6 +96,14 @@ class pong(models.Model):
 					paddleL_speed = -6
 				if event.key == pygame.K_s:
 					paddleL_speed = 6
+				if event.key == pygame.K_l:
+					paddleR.y -= 1
+				if event.key == pygame.K_o:
+					paddleR.y += 1
+				if event.key == pygame.K_t:
+					speed = 6
+				if event.key == pygame.K_y:
+					speed = 120
 			if event.type == pygame.KEYUP:
 				if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
 					paddleR_speed = 0
@@ -86,13 +111,6 @@ class pong(models.Model):
 				if event.key == pygame.K_w or event.key == pygame.K_s:
 					paddleL_speed = 0
 
-	#update screen (frontend will have to handle that eventually)
-		pygame.display.update()
-		clock.tick(60)
-		screen.fill("black")
-		pygame.draw.rect(screen,'white',ball)
-		pygame.draw.rect(screen,'white',paddleL)
-		pygame.draw.rect(screen,'white',paddleR)
 
 		#move ball
 		ball.x += ball_speedX
@@ -130,43 +148,69 @@ class pong(models.Model):
 			paddleR.y = 0
 
 
-		gamestate[0] = paddleL.y
-		gamestate[1] = paddleR.y
-		gamestate[2] = ball.x
-		if gamestate[2] < 0:
-			gamestate[2] = 0
-		if gamestate[2] > screen_width:
-			gamestate[2] = screen_width
-		gamestate[3] = ball.y
+		gamestate[1] = paddleL.y
+		gamestate[2] = paddleR.y
+		gamestate[3] = ball.x
+		if gamestate[3] < 0:
+			gamestate[3] = 0
+		if gamestate[3] > screen_width:
+			gamestate[3] = screen_width
+		gamestate[4] = ball.y
 
-
-		#make ball bounce on top and bottom
-		if ball.y <= 0 or ball.y >= screen_height:
-			ball_speedY *= -1
-		#make ball reset if i leaves screen on x axis
-		if ball.x > screen_width or ball.x < 0:
-			ball.x = screen_width/2
-			ball.y = screen_height/2
-			ball_speedY = ball_start_speedy
-			ball_speedX = ball_start_speedx
-			if ball.x > screen_width:
-				Lscore += 1
-				gamestate[4] = Lscore
-			if (ball.x < 0):
-				Rscore += 1
-				gamestate[5] = Rscore
-
-		if ball.colliderect(paddleL):
+		#make ball bounce on paddles
+		#Left Paddle
+		if(ball.x < 0 + paddle_width and (ball.y >= paddleL.y - ball_sizey and ball.y <= paddleL.y + paddle_heigth)):
+			ball.x = 0 + paddle_width
+			passes += 1
 			if paddleL_speed > 0:
 				ball_speedY += ball_bounce_mult
 			if paddleL_speed < 0:
 				ball_speedY -= ball_bounce_mult
 			ball_speedX *= -1
-		if ball.colliderect(paddleR):
+		#Right Paddle
+		if(ball.x > screen_width - paddle_width - ball_sizex and (ball.y + ball_sizex >= paddleR.y and ball.y <= paddleR.y + paddle_heigth)):
+			ball.x = screen_width - paddle_width - ball_sizex
+			passes += 1
 			if paddleR_speed > 0:
 				ball_speedY += ball_bounce_mult
 			if paddleR_speed < 0:
 				ball_speedY -= ball_bounce_mult
 			ball_speedX *= -1
 
+		#make ball bounce on top and bottom
+		if ball.y <= 0 or ball.y >= screen_height - ball_sizey:
+			ball_speedY *= -1
+		#make ball reset if i leaves screen on x axis
+		if ball.x > screen_width - ball_sizex or ball.x < 0:
+			print("score!")
+		if ball.x > screen_width - ball_sizex:
+			Lscore += 1
+			gamestate[5] = Lscore
+			ball_speedX = ball_start_speedx
+			print(Lscore)
+			ball.x = screen_width/2
+			ball.y = screen_height/2
+			ball_speedY = ball_start_speedy
+		if (ball.x < 0):
+			Rscore += 1
+			gamestate[6] = Rscore
+			ball_speedX = -ball_start_speedx
+			print(Rscore)
+			ball.x = screen_width/2
+			ball.y = screen_height/2
+			ball_speedY = ball_start_speedy
+
+	#update screen (frontend will have to handle that eventually)
+		nonce = int(time.time() * 1000) - start
+		gamestate[0] = nonce
+
+		print(nonce)
+
+		pygame.display.update()
+		clock.tick(speed)
+		screen.fill("black")
+		pygame.draw.rect(screen,'white',ball)
+		pygame.draw.rect(screen,'white',paddleL)
+		pygame.draw.rect(screen,'white',paddleR)
 		log.write(' '.join(str(x) for x in gamestate) + "\n")
+
