886ecb9e2ef9092ed7fa350de4a3a724986a5dfa
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2024-11-26 12:26:32 +0100

    minor changes

--- src/userdata/app/hello_django/urls.py
+++ src/userdata/app/hello_django/urls.py
@@ -26,10 +26,9 @@ from upload.views import image_upload
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", image_upload, name="upload"),
-    path('users/', include('users.urls')),
-    path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
-
+    path('user-api/users/', include('users.urls')),
+    path("user-api/login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("user-api/logout/", LogoutView.as_view(next_page="/"), name="logout"),
 
 ]
 
