ad6c09d5f2c15be5392b361dc678975bb929ddfc
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2024-12-13 13:26:35 +0100

    added more features to user management

--- src/userdata/app/hello_django/settings.py
+++ src/userdata/app/hello_django/settings.py
@@ -141,6 +141,10 @@ USE_TZ = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
 
+CSRF_COOKIE_HTTPONLY = False
+
+CSRF_COOKIE_SECURE = False
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
