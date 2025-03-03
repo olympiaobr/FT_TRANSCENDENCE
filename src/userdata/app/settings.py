bd5864f797fdc52af78a1f130c878a1ea0cf6162
yubi42 <undercoverbabe12@gmail.com>
2025-03-03 14:38:07 +0100

    increase access token lifetime

--- src/userdata/app/settings.py
+++ src/userdata/app/settings.py
@@ -77,7 +77,7 @@ REST_FRAMEWORK = {
 ROOT_URLCONF = 'app.urls'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=150),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
